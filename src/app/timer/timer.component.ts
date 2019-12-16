1cb1dc1927a735b4752a15ccf931b9da47dda219
Andrew <aandrewnngo@gmail.com>
2019-12-16 01:40:35 -0600

    timer implementation with an inspection time before starting

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
@@ -7,10 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
-  timerDisplay = "0.00";
+  inspectionTime = 15;
+  seconds = 0.00;
+  timeID;
+  inspectionID;
+  running: number = 0;
+  key;
   constructor() { }
+
+  @HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event:KeyboardEvent) {
+    this.key = event.key;
+    if(this.key === ' '){
+      this.start();
+    }
+  }
 
   ngOnInit() {
   }
 
+  startTimer()
+  {
+    clearInterval(this.inspectionID);
+    this.seconds = 0.00;
+    this.running = 2;
+    this.timeID = setInterval(() => {
+      this.seconds += .01;
+    }, 10);
+    
+  }
+  stopTimer(){
+    this.running = 0;
+    this.inspectionTime = 15;
+    clearInterval(this.timeID);
+  }
+  start(){
+    this.running += 1;
+    if(this.running > 2){
+      this.running = 0;
+    }
+    switch(this.running){
+      case 0: 
+        this.stopTimer();
+        break;
+      case 1:
+        this.inspection();
+        break;
+      case 2:
+        this.startTimer();
+        break;
+    }
+  }
+  inspection(){
+    this.inspectionID = setInterval(() => {
+        this.inspectionTime -= 1;
+        if(this.inspectionTime === 0){
+          this.startTimer();
+        }
+      }, 1000);
+  }
 }
